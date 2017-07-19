58dcb634017b5560f1a47e3ad4d989fb13593715
Rudys Natanael Acosta <natanael926@gmail.com>
2017-07-19 16:55:24 -0400

    Inicio proyecto

---
+++ src/Models/Helado.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace "App/Models";
+
+class Helado
+{
+
+	protected $helados = [];
+
+	protected $name;
+
+	protected $price;
+
+	protected $image;
+
+	public function getHelados() : array
+	{
+		return [];
+	}
+
+}
